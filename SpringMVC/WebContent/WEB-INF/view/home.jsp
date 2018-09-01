7579d3c4ff8d05f164dabe3c5fc23458d6bf3ecf
WEBEXZ <audi.icon@gmail.com>
2018-09-01 14:06:59 -0500

    Starting Project

---
+++ SpringMVC/WebContent/WEB-INF/view/home.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Spring MVC</title>
+</head>
+<body>
+	<h2>Home</h2>
+</body>
+</html>
---
+++ SpringMVC/src/com/webexz/springmvc/HomeController.java
@@ -0,0 +1,13 @@
+package com.webexz.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String showPage() {
+		return "home";
+	}
+}
